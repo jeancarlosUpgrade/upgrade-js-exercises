eb4950113b39cd66fae27dbcc915dbf87f297c2c
jean <jean.carlos@bootcamp-upgrade.com>
2021-12-22 02:25:48 +0100

    final commit

--- upgrade-asynchronous/iteracion2/index.js
+++ upgrade-asynchronous/iteracion2/index.js
@@ -3,17 +3,17 @@
 // ```jsx
 // 2.1 Convierte la siguiente promesa para esperar a ejecutar el console usando
 // async-await.
-// const runTimeOut = async () => {
-//   await new Promise((resolve) => {
-//     setTimeout(function () {
-//       resolve();
-//     }, 2000);
-//   });
+const runTimeOut = async () => {
+  await new Promise((resolve) => {
+    setTimeout(function () {
+      resolve();
+    }, 2000);
+  });
 
-//   console.log("Time out!");
-// };
+  console.log("Time out!");
+};
 
-// runTimeOut();
+runTimeOut();
 
 // 2.2 Convierte la siguiente función con un fetch utilizando async-await.
 // Recuerda que para usar .fetch() tendrás que probar el ejercicio en el navegador;
